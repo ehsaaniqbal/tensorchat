e55d368b3d9bbec2b5df6cd19e258131b46a8278
ehsaan <ehsaaniqbal@gmail.com>
2019-09-07 18:47:49 +0400

    build v2.1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 //middleware
 app.set("views", "./views");
