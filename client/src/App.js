13498c86b8ad6897c16165c3538f1406290a3337
ehsaan <ehsaaniqbal@gmail.com>
2019-09-26 22:27:16 +0400

    TENSORCHAT V3

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./assets/css/App.css";
+import "./assets/css/blk-design-system-react.css.map";
 import "./assets/css/blk-design-system-react.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home.jsx";
 import CreateRoom from "./containers/CreateRoom";
 import Room from "./containers/Room";
-
+const NoMatchPage = () => {
+  return (
+    <h3>404 - Not found</h3>
+  );
+};
 
 function App() {
   return (
@@ -14,7 +19,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/createroom" component={CreateRoom} />
         <Route exact path="/r/:roomname" component={Room} />
-    
+        <Route component={NoMatchPage} />
       </Switch>
     </BrowserRouter>
   );
